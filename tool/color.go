9061866ee7cbcbf53f0b8ff10e4c46156d357580
Joeky <joeky5888@gmail.com>
2020-06-21 03:06:04 +0800

    Use cross platform color
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- tool/color.go
+++ tool/color.go
@@ -12,13 +12,14 @@ type Conf struct {
 
 const (
 	Default = "%s"
-	Black   = "\033[0;30m%s\033[0m"
-	Red     = "\033[1;31m%s\033[0m"
-	Green   = "\033[1;32m%s\033[0m"
-	Yellow  = "\033[1;93m%s\033[0m"
-	Purple  = "\033[1;34m%s\033[0m"
-	Magenta = "\033[1;35m%s\033[0m"
-	Blue    = "\033[1;36m%s\033[0m"
-	White   = "\033[1;37m%s\033[0m"
-	Gray    = "\033[1;90m%s\033[0m"
+	Red     = "\033[38;05;160m%s\033[0m"
+	Green   = "\033[38;05;40m%s\033[0m"
+	Yellow  = "\033[38;05;11m%s\033[0m"
+	Purple  = "\033[38;05;93m%s\033[0m"
+	Magenta = Red
+	Blue    = "\033[38;05;14m%s\033[0m"
+	Cyan    = Blue
+	Gray    = "\033[38;05;8m%s\033[0m"
+	// Black   = "\033[0;30m%s\033[0m"
+	// White   = "\033[1;37m%s\033[0m"
 )
